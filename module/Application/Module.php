8cd0eea6f6129ac842309a361d58e1e1aaf46abc
Walter Simões <walterssimoes@gmail.com>
2015-05-01 20:40:50 -0300

    M3Ex1

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -19,6 +19,13 @@ class Module
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
+        $eventManager->attach(MvcEvent::EVENT_DISPATCH, array($this, 'onDispatch'), 100);
+    }
+    
+    public function onDispatch(MvcEvent $e)
+    {
+        $vm = $e->getViewModel();
+        $vm->setVariable("categories", "CATEGORY LIST");
     }
 
     public function getConfig()
